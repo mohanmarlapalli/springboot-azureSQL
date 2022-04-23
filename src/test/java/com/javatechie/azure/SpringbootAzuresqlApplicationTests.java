9d696f329236dadfb928657d0df104278511215c
mohanmarlapalli <30617999+mohanmarlapalli@users.noreply.github.com>
2022-04-23 19:09:50 +0530

    Update SpringbootAzuresqlApplicationTests.java

--- src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
+++ src/test/java/com/javatechie/azure/SpringbootAzuresqlApplicationTests.java
@@ -1,13 +1,13 @@
-package com.javatechie.azure;
+//package com.javatechie.azure;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SpringbootAzuresqlApplicationTests {
+//@SpringBootTest
+//class SpringbootAzuresqlApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
-}
+//}
