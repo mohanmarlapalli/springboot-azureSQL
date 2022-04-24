c7fc296b9bb8ec01f9917190470058a834e33bb4
mohanmarlapalli <30617999+mohanmarlapalli@users.noreply.github.com>
2022-04-24 07:52:30 +0530

    Update Employee.java

--- src/main/java/com/javatechie/azure/Employee.java
+++ src/main/java/com/javatechie/azure/Employee.java
@@ -1,8 +1,5 @@
 package com.javatechie.azure;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -11,9 +8,6 @@ import javax.persistence.Table;
 
 @Entity
 @Table
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue
@@ -21,4 +15,30 @@ public class Employee {
     private String name;
     private String dept;
     private long salary;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
+	}
+	public long getSalary() {
+		return salary;
+	}
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+    
+    
 }
